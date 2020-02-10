dd047e01c2ded48af72d859bb469f5dcca5423da
Stephanie Ballard <user.name>
2020-02-10 13:44:47 -0800

    RecipeDetailViewController set up

--- Recipes/Recipes/ViewController/RecipeDetailViewController.swift
+++ Recipes/Recipes/ViewController/RecipeDetailViewController.swift
@@ -11,15 +11,31 @@ import UIKit
 class RecipeDetailViewController: UIViewController {
 
     @IBOutlet weak var recipeNameLabel: UILabel!
-    
     @IBOutlet weak var recipeTextView: UITextView!
+    
+    //In the RecipeDetailViewController, add a variable recipe: Recipe?.
+    //Call updateViews() in viewDidLoad() and in didSet of the recipe variable.
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
 
         // Do any additional setup after loading the view.
     }
     
+    //Create a function called updateViews(). This should take the values of the recipe and place them in the corresponding outlets. The recipe's name should go in the label, and the instructions in the text view. Check that the view is loaded when unwrapping the recipe object by using the view controller's isViewLoaded property.
+    func updateViews() {
+        if self.isViewLoaded, let recipe = recipe {
+            
+            recipeNameLabel.text = recipe.name
+            recipeTextView.text = recipe.instructions
+        }
+    }
 
     /*
     // MARK: - Navigation
